00611636839431c6f6fbc42b1ff3b0845072f7f1
nosharp <nosharp1@gmail.com>
2021-03-22 22:02:16 +0000

    Fix: Remove unnecessary imports from QuizapiApplication

--- src/main/java/cc/nosharp/quizapi/QuizapiApplication.java
+++ src/main/java/cc/nosharp/quizapi/QuizapiApplication.java
@@ -1,10 +1,6 @@
 package cc.nosharp.quizapi;
 
 import cc.nosharp.quizapi.data.RedisHandler;
-import cc.nosharp.quizapi.datamodels.Question;
-import cc.nosharp.quizapi.datamodels.QuestionList;
-import cc.nosharp.quizapi.datamodels.QuestionListMediator;
-import cc.nosharp.quizapi.opentdb.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
