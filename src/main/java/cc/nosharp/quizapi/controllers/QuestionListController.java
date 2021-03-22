bcf8e3a0ffb609a057775baf7444e8582a547068
nosharp <nosharp1@gmail.com>
2021-03-22 13:50:47 +0000

    Feat: Added GetNewQuestion Endpoint

--- src/main/java/cc/nosharp/quizapi/controllers/QuestionListController.java
+++ src/main/java/cc/nosharp/quizapi/controllers/QuestionListController.java
@@ -1,17 +1,23 @@
 package cc.nosharp.quizapi.controllers;
 
 import cc.nosharp.quizapi.datamodels.QuestionList;
+import cc.nosharp.quizapi.datamodels.QuestionListMediator;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.context.request.async.DeferredResult;
 
 @Controller
 public class QuestionListController {
 
+    @Async
     @GetMapping("/api/v1/GetNewQuestionList")
     @ResponseBody
-    private QuestionList getNewQuestionList(){
+    public DeferredResult<QuestionList> getNewQuestionList(){
+        DeferredResult<QuestionList> out = new DeferredResult<>();
+        QuestionListMediator.getQuestionList().createNewQuesitonList(out::setResult);
 
-        return null;
+        return out;
     }
 }
