23a8423d1c996fd9a329f110f71e697d6fbfee8a
nosharp <nosharp1@gmail.com>
2021-04-07 10:46:04 +0100

    feat: Add testing for redis handler.

---
+++ src/test/java/cc/nosharp/quizapi/data/RedisHandlerTest.java
@@ -0,0 +1,59 @@
+package cc.nosharp.quizapi.data;
+
+
+import cc.nosharp.quizapi.datamodels.QuestionList;
+import cc.nosharp.quizapi.opentdb.OpenTDBAPI;
+import cc.nosharp.quizapi.opentdb.TDBAPICategory;
+import cc.nosharp.quizapi.opentdb.TDBAPIQuestionType;
+import net.jodah.concurrentunit.Waiter;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Logger;
+
+public class RedisHandlerTest {
+
+    private static boolean ran = false;
+
+    @Test
+    public void getNewUUIDTest(){
+        RedisHandler handler = RedisHandler.getRedisHandler();
+        Assertions.assertEquals(handler.getNewUUID().length(), 36);
+    }
+
+    @Test
+    public void doesUUIDExistTest() throws Throwable {
+        RedisHandler handler = RedisHandler.getRedisHandler();
+        String UUID = handler.getNewUUID();
+        CountDownLatch countDownLatch = new CountDownLatch(1);
+        final Waiter waiter = new Waiter();
+        OpenTDBAPI.GetNewQuestions(10, TDBAPICategory.SCIENCE_COMPUTER, TDBAPIQuestionType.TRUEFALSE, callback ->{
+            QuestionList generatedQuestions = QuestionList.fromTDBAPI(UUID, callback);
+            handler.insertNewQuestionList(generatedQuestions);
+            waiter.resume();
+        });
+        waiter.await(15000);
+        Assertions.assertTrue(handler.doesUUIDExist(UUID));
+
+    }
+
+    @Test
+    public void canDeleteQuestionListTest() throws Throwable{
+        RedisHandler handler = RedisHandler.getRedisHandler();
+        String UUID = handler.getNewUUID();
+        CountDownLatch countDownLatch = new CountDownLatch(1);
+        final Waiter waiter = new Waiter();
+        OpenTDBAPI.GetNewQuestions(10, TDBAPICategory.SCIENCE_COMPUTER, TDBAPIQuestionType.TRUEFALSE, callback ->{
+            QuestionList generatedQuestions = QuestionList.fromTDBAPI(UUID, callback);
+            handler.insertNewQuestionList(generatedQuestions);
+            waiter.resume();
+        });
+        waiter.await(15000);
+        Assertions.assertTrue(handler.doesUUIDExist(UUID));
+        handler.deleteGameUUID(UUID);
+        Assertions.assertFalse(handler.doesUUIDExist(UUID));
+    }
+}
